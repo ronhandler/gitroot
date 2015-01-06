26645ec5dfb24832ddf6884d0c1d063f7a61a3e5
ronhandler <ronhandler@gmail.com>
2015-01-07 00:46:58 +0200

    boostgraph is a project that uses the boost graph library to create a graph. It also uses GraphViz to save the graph as an image, and the program "feh" to display this image.

---
+++ boostgraph/src/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <deque>
+#include <iterator>
+using namespace std;
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/topological_sort.hpp>
+#include <boost/graph/graphviz.hpp>
+using namespace boost;
+
+#endif
+
