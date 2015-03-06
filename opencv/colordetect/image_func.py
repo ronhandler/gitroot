c35ff52f97c7e820230aa0ae66b666cbd3708ab1
ronhandler <ronhandler@gmail.com>
2015-03-06 11:48:16 +0200

    Fixed both files. They should now work together

--- opencv/colordetect/image_func.py
+++ opencv/colordetect/image_func.py
@@ -1,44 +1,45 @@
-#!/usr/bin/python
 
 import cv2
 import math
 import numpy as np
 
-prev_i = cv2.imread("/shared/prev_copy.jpg")
-state_i = cv2.imread("/shared/new_copy.jpg")
-new_i = cv2.imread("/shared/new_copy.jpg")
+def diff_func(prev_i,state_i):
+    #new_i = cv2.imread("/shared/new_copy.jpg")
+    # This is a weird way to define new_i as an image...
+    new_i = prev_i
 
-THRESHOLD = 5
+    #THRESHOLD = input('Please enter your requested level of Threshold')
+    THRESHOLD = 5
 
-height, width = prev_i.shape[:2]
+    height, width = prev_i.shape[:2]
+    i = 0
+    j = 0
+    #width = width - 1
+    #height = height - 1
+    for i in range (0,width):
+        for j in range (0,height):
+            if compare_pixel(state_i[j,i],prev_i[j,i],THRESHOLD):
+                new_i[j,i] = state_i[j,i]
+            else:
+                new_i[j,i] = [0,0,0]
 
-i = 0
-j = 0
-
+    #new_i = cv2.cvtColor(new_i, cv2.COLOR_BGR2GRAY)
+    return new_i
 
 def absval(qt):
-	if qt<0:
-		qt = qt/-1
-		return qt
-	return qt
+    if qt<0:
+        qt = qt/-1
+        return qt
+    return qt
 
+def trans_scalar_level(input_num,THRESHOLD):
+    x = (input_num/THRESHOLD)*10
+    return round(x)
 
 def compare_pixel(rhs,lhs,THRESHOLD):
-	#check = absval(int(rhs[0])-int(lhs[0]))
-	#check += absval(int(rhs[1])-int(lhs[1]))
-	#check += absval(int(rhs[2])-int(lhs[2]))
-	#if check <= THRESHOLD:
-	if rhs[0]==lhs[0] and rhs[1]==lhs[1] and rhs[2]==rhs[2]:
-		return 0
-	return 1
-
-for i in range (0,width):
-        for j in range (0,height):
-                if compare_pixel(state_i[i,j],prev_i[i,j],THRESHOLD):
-                        new_i[i,j] = state_i[i,j]
-                else:
-                        new_i[i,j] = [0,0,0]
-
-#new_i = cv2.cvtColor(new_i, cv2.COLOR_BGR2GRAY)
-
-cv2.imwrite("diff.jpg",new_i)
+    check = absval(int(rhs[0])-int(lhs[0])) + absval(int(rhs[1])-int(lhs[1])) + absval(int(rhs[2])-int(lhs[2]))
+    if check <= THRESHOLD:
+        #if rhs[0]==lhs[0] and rhs[1]==lhs[1] and rhs[2]==rhs[2]:
+        return 0
+    return 1
+    #return trans_scalar_level(check,THRESHOLD)
--- opencv/colordetect/main.py
+++ opencv/colordetect/main.py
@@ -2,11 +2,28 @@
 import cv2
 import numpy as np
 from ColourDetection import *
+from image_func import *
 
-image = np.zeros((512, 512, 3), np.uint8)
-image[:,0:512] = (0, 255, 0) # Green in BGR.
+#image = np.zeros((512, 512, 3), np.uint8)
+#image[:,0:512] = (0, 255, 0) # Green in BGR.
 #image[:,0:512] = (0, 0, 255) # Red in BGR.
 #image[:,0:512] = (255, 0, 0) # Blue in BGR.
-color = ColourDetection.detect_color(image)
+
+cap = cv2.VideoCapture(0)
+ret, new_image = cap.read()
+
+old_image = cv2.imread('old.jpg')
+
+
+height1, width1 = old_image.shape[:2]
+height2, width2 = new_image.shape[:2]
+
+#print("The sizes are:" + str(width1) + "x" + str(height1))
+#print("The sizes are:" + str(width2) + "x" + str(height2))
+
+diff_image = diff_func(new_image, old_image);
+
+color = ColourDetection.detect_color(diff_image)
 print("The returned color is: " + color)
-#cv2.imwrite('output.jpg', image)
+cv2.imwrite('old.jpg', new_image)
+
