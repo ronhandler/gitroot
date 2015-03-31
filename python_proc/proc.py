d3b9e2cc43f398a6f92a6dc72f63b5bcc203a73f
ronhandler <ronhandler@gmail.com>
2015-04-01 02:03:10 +0300

    Another version of python_proc. Added multithreading support with timeout

--- python_proc/proc.py
+++ python_proc/proc.py
@@ -14,21 +14,54 @@ import subprocess
 import signal
 
 # Tries to run a process, but times out after some time.
-def call(args, timeout=0):
-	proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	try:
-		signal.signal(signal.SIGALRM, alarm_handler)
-		signal.alarm(timeout)  # Default timeout is 60 seconds.
-		result = proc.communicate()[0].decode('utf-8').strip()
-		signal.alarm(0)  # Reset the alarm.
-	except Alarm:
-		# If the process timed out, kill it.
-		proc.kill()
-		raise Alarm("Execution timed out after " + str(timeout) + " seconds.  Command: " + args)
-	return result, proc.returncode
+def call_command(args, timeout=0, exception=False):
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    try:
+        signal.signal(signal.SIGALRM, alarm_handler)
+        signal.alarm(timeout)  # Default timeout is 60 seconds.
+        result = proc.communicate()[0].decode('utf-8').strip()
+        signal.alarm(0)  # Reset the alarm.
+        return result, proc.returncode
+    except Alarm:
+        # If the process timed out, kill it.
+        proc.kill()
+        if exception == True:
+            raise Alarm("Execution timed out after " + str(timeout) + " seconds.  Command: " + args)
+        else:
+            return ("", -1)
 
 class Alarm(Exception):
-	pass
+    pass
 
 def alarm_handler(signum, frame):
-	raise Alarm
+    raise Alarm
+
+import multiprocessing
+import multiprocessing.pool
+
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
+def wrapper(cmd, timeout=None):
+    with multiprocessing.Pool(processes=1) as pool:
+        try:
+            result = []
+            r = pool.map_async(call_command, [cmd], callback=result.append)
+            r.get(timeout)
+            return result[0][0]
+        except multiprocessing.TimeoutError:
+            return (None,-1)
+
+def call(cmd, timeout=None):
+    return wrapper(cmd,timeout)
+
--- python_proc/test.py
+++ python_proc/test.py
@@ -1,10 +1,19 @@
-#!/usr/bin/python -B
+#!/usr/bin/python3 -B
 
 import sys
 import proc
 
 if __name__ == "__main__":
-    string,_ = proc.call("""for i in `seq 1 10`; do echo $i; done""")
-    print(string)
+
+    cmds = [
+        ("sleep 1;echo hello world",None),
+        ("echo hi there",2),
+    ]
+
+    with proc.MyPool(processes=4) as pool:
+        results = pool.starmap(proc.call, cmds)
+        for i in results:
+            print(i)
+
     sys.exit(0);
 
