c76e0034a9c9c40dd35b6af083ac6ac3ee1b05ac
ronhandler <ronhandler@gmail.com>
2015-11-22 17:22:43 +0200

    Adding some initial files

---
+++ bgammon/python/bgammon.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+from board import *
+
+if __name__ == '__main__':
+    b = Board()
+    print(b)
+
---
+++ bgammon/python/board.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+class Board(object):
+    def __init__(self):
+        pass
+    def __str__(self):
+        return "hello!"
